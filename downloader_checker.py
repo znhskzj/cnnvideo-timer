5a682acee4ed19a35609c3d16f093c702f75cc66
Rong Zhu <wuxiami@hotmail.com>
2023-11-01 11:42:33 -0400

    checking the availability of new videos and managing their download process

--- downloader_checker.py
+++ downloader_checker.py
@@ -1,13 +1,17 @@
-# downloader_checker.py v2.4.1
+"""
+ downloader_checker.py v2.4.1
 
-# This script is responsible for checking the availability of new videos and managing their download process. It utilizes the video_downloader module to perform the actual download, and it ensures that each video is only downloaded once by checking against a record of previously downloaded videos.
+This script is responsible for checking the availability of new videos and managing their download process. It utilizes the video_downloader module to perform the actual download, and it ensures that each video is only downloaded once by checking against a record of previously downloaded videos.
+"""
 
 import time
 import os
 import logging
-import random  # Importing random to introduce randomness in retry intervals
+import random  
+from typing import List, Tuple, Any, Optional
+
 import yt_dlp
-from typing import Any, Optional
+
 from utils import sanitize_filename
 from config_loader import load_config
 from metadata_manager import MetadataManager
@@ -18,22 +22,25 @@ config = load_config()
 
 class DownloaderManager:
 
-    def __init__(self, videos, downloader, config):
+    def __init__(self, videos: List[dict], downloader: Any, config: dict) -> None:
         """Initialize the DownloaderManager with videos to be downloaded, a downloader, and configuration.
         
-        Parameters:
+        Args:
         - videos : list : A list of video information to be downloaded.
         - downloader : YTDownloader : An instance of YTDownloader to perform the actual download.
         - config : dict : Configuration parameters.
+        
+        Returns:
+        - None
         """
         self.videos = videos
         self.downloader = downloader
         self.metadata_manager = MetadataManager(config)  # 创建MetadataManager的实例，并传递配置
 
-    def should_download(self, video_info):
+    def should_download(self, video_info: dict) -> bool:
         """Determine if a video should be downloaded based on its title and metadata.
         
-        Parameters:
+        Args:
         - video_info : dict : Information about the video to be downloaded.
         
         Returns:
@@ -45,8 +52,15 @@ class DownloaderManager:
         # Check if the file exists in the file system and metadata exists.
         return not (os.path.exists(video_path) and self.metadata_manager.query_metadata(video_info['id']))
 
-    def store_video_metadata(self, video_info):
-        """Store video metadata using MetadataManager."""
+    def store_video_metadata(self, video_info: dict) -> None:
+        """Store video metadata using MetadataManager.
+
+        Parameters:
+        - video_info : dict : Information about the video to be downloaded.
+
+        Returns:
+        - None
+        """
         sanitized_title = sanitize_filename(video_info['title'])
         video_path = os.path.join(self.downloader.output_directory, sanitized_title + '.mp4')
         metadata = {
@@ -61,8 +75,16 @@ class DownloaderManager:
 
         self.metadata_manager.save_or_update_metadata(metadata)
 
-    def check_and_download(self):
-        downloaded_filenames = []  # 用于存储已下载视频的文件名
+    def check_and_download(self) -> List[str]:
+        """Prepare to download videos and return a list of filenames of downloaded videos.
+
+        Args:
+        - None
+                
+        Returns:
+        - List[str] : A list of filenames of the downloaded videos.
+        """
+        downloaded_filenames = []  # Used to store filenames of downloaded videos
         downloaded_count = 0
         # downloaded_titles = []  
 
@@ -70,19 +92,19 @@ class DownloaderManager:
 
         for video_url in self.videos:
             try:
-                downloaded_title, cleaned_filename = self._download_single_video(video_url)  # 获取原始标题和清理过的文件名
-                if cleaned_filename:  # 如果有清理过的文件名，表示视频已成功下载
-                    downloaded_filenames.append(cleaned_filename)  # 保存清理过的文件名
+                downloaded_title, cleaned_filename = self._download_single_video(video_url)  # Get original title and cleaned filename
+                if cleaned_filename: # If there's a cleaned filename, the video has been successfully downloaded
+                    downloaded_filenames.append(cleaned_filename)   # Save cleaned filename
                     downloaded_count += 1
             except Exception as e:
                 logger.error(f"Failed to download video from {video_url}. Error: {e}.", exc_info=True)  # Logging more error info
                 
         return downloaded_filenames
     
-    def _download_single_video(self, video_url: str) -> Optional[str]:
+    def _download_single_video(self, video_url: str) -> Tuple[Optional[str], Optional[str]]:
         """Try downloading a single video and return the title if successful.
         
-        Parameters:
+        Args:
         - video_url : str : URL of the video to be downloaded.
         
         Returns:
@@ -115,7 +137,15 @@ class DownloaderManager:
         logger.error(f"Failed to download video from {video_url} after {max_retries} retries.")
         return None, None  # Return None if the download failed after retries
     
-    def get_suitable_formats(self, video_url):
+    def get_suitable_formats(self, video_url: str) -> List[str]:
+        """Get a list of suitable format IDs for a given video URL.
+        
+        Arus:
+        - video_url : str : The URL of the video to get suitable formats for.
+
+        Returns:
+        - List[str] : A list of strings representing suitable format IDs for the video, sorted by file size.
+        """
         max_resolution = self.config.get('MAX_RESOLUTION') 
         ydl_opts = {
         'quiet': True,
@@ -127,29 +157,34 @@ class DownloaderManager:
             info_dict = ydl.extract_info(video_url, download=False)
             formats = info_dict.get('formats', [])
             
-            # 筛选符合条件的格式
             suitable_formats = [
                 f for f in formats 
                 if f['ext'] == 'mp4' and f.get('height') and f['height'] <= max_resolution
             ]
             
-            # 按文件大小排序
             suitable_formats.sort(key=lambda f: f['filesize'] or 0)
             
-            # 获取格式ID列表
             format_ids = [str(f['format_id']) for f in suitable_formats]
             
             return format_ids
         
-    def download_video_with_format(self, video_url):
+    def download_video_with_format(self, video_url: str) -> None:
+        """Download a video in a suitable format from a list of format IDs.
+        
+        Args:
+        - video_url : str : The URL of the video to be downloaded.
+
+        Returns:
+        - None
+        """
         format_list = self.get_suitable_formats(video_url)
         
         for fmt in format_list:
             try:
-                # 尝试下载
+                 # Attempt to download
                 self.downloader.download_video(video_url, fmt)
-                # 如果下载成功，退出循环
+                # If download is successful, exit the loop
                 break
             except Exception as e:
-                # 如果下载失败，继续尝试其他格式
+                # If download fails, continue trying with other formats
                 logger.error(f"Error downloading {video_url} with format {fmt}: {e}")
